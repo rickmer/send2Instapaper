354033504b77cf46c00581abd8bc21446b729540
Rickmer Frier <me@rickmer.org>
2013-02-20 00:36:11 +0100

    iCandy ist auch drin

--- Send2Instapaper.safariextension/inject_script.js
+++ Send2Instapaper.safariextension/inject_script.js
@@ -4,30 +4,44 @@ function handleMessage(msgEvent) {
 
     if (messageName === "info") {
         if (messageData === "nouserdata") {
-            alert("You need to set your userdata!");
+            showInstaInfo('You need to set your userdata!');
         }
         if (messageData === "saving") {
-            alert("saving...");
+            showInstaInfo('saving...');
         }
     }
 
     if (messageName === "api") {
         if (messageData === "403") {
-            alert("Invalid username or password.");
+            showInstaInfo('Invalid username or password.');
         }
         if (messageData === "201") {
-            alert("saved");
+            showInstaInfo('saved');
         }
         if (messageData === "400") {
-            alert("Rate limit exceeded.");
+            showInstaInfo('Rate limit exceeded.');
         }
-
     }
-
-
 }
 
+function showInstaInfo(message) {
+    setInstaDiv(message);
+    setTimeout('setInstaDiv("")',800);
+}
 
-
+function setInstaDiv(text) {
+    if (text === "") {
+        document.getElementById('instapaper_info').setAttribute('class','inactive_insta_info');
+        document.getElementById('instapaper_info').innerHTML = '<div id="instaButton">' + text + '</div>';
+    } else {
+        document.getElementById('instapaper_info').setAttribute('class','active_insta_info');
+        document.getElementById('instapaper_info').innerHTML = '<div id="instaButton">' + text + '</div>'; 
+    }
+}
 
 safari.self.addEventListener("message", handleMessage, false);
+
+var newNode = document.createElement('div');
+newNode.setAttribute('id','instapaper_info');
+document.body.appendChild(newNode);
+
